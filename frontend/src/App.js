3c4697053b0695e20242b004848d213df69dbdf5
Chi-Hung, Chuang <ko841215@gmail.com>
2021-12-25 18:17:01 +0800

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,10 @@ import SignIn from './Containers/SignIn';
 import SignUp from './Containers/SignUp';
 import Logined from './Containers/Logined';
 import { useEffect, useState } from "react";
-import useChat from "../hooks/useChat";
 
 
 function App() {
-  const {
-    userStatus,
-    setUserStatus,
-    email,
-    setEmail,
-    userName,
-    setUserName,
-    password,
-    setPassword,
-  } = useChat();
+  const [userStatus, setUserStatus] = useState("login");
 
   return (
     <>
