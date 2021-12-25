9208fc60a3bf86063126aaf32005df6b6c865214
Chi-Hung, Chuang <ko841215@gmail.com>
2021-12-25 15:10:15 +0800

    add frontend folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './Containers/SignIn';
+import SignUp from './Containers/SignUp';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [signIn, setSignIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {signIn ? <SignIn /> : <SignUp />} 
+    </>
   );
 }
 
