188741183188e4d0924c9ce32bc85d08a3acfa8c
Chi-Hung, Chuang <ko841215@gmail.com>
2022-01-01 20:42:16 +0800

    merge

---
+++ frontend/src/Hooks/useChatBox.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const useChatBox = () => {
+    const [chatBoxes, setChatBoxes] = useState([]);
+    const removeChatBox = (targetKey, activeKey) => {
+        let temp = chatBoxes;
+        let i = temp.indexOf(targetKey);
+        temp.splice(targetKey, 1);
+        setChatBoxes(temp);
+        if(targetKey === activeKey){
+            if(i > 0){
+                return temp[i];
+            }else{
+                if(temp.length){
+                    return temp[0];
+                }else {
+                    return '';
+                }
+            }
+        }
+    };
+    const createChatBox = (friend) => {
+        setChatBoxes([...chatBoxes, friend]);
+    };
+    return {chatBoxes, removeChatBox, createChatBox};
+};
+
+
+export default useChatBox;
+
+
