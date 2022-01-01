a20a6969c8f2b58c0ccf793fd597dd81c0705c33
Whyu1125 <kobe7682477@gmail.com>
2022-01-01 23:40:35 +0800

    1

---
+++ frontend/src/Components/Control.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Control = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  // border: solid;
+`;
+
+export default Control;
---
+++ frontend/src/Components/Display.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Display = styled.div`
+    width: 100%;
+    height: 80%;
+    // border: solid;
+`;
+
+export default Display;
