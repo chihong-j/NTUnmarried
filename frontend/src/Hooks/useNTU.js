62d05d0298bcb5e877c8128ee35978cd21a843bc
Whyu1125 <kobe7682477@gmail.com>
2022-01-08 23:27:35 +0800

    1

--- frontend/src/Hooks/useNTU.js
+++ frontend/src/Hooks/useNTU.js
@@ -7,8 +7,20 @@ const useNTU = () => {
     const [gender, setGender] = useState(true); //{true: male, false: female}
     const [age, setAge] = useState(0);
     const [birth, setBirth] = useState(new Date());
+    const initialize = (me) => {
+        if(me.images){
+            setIamges(me.images.concat(new Array(6 - me.images.length)))
+        }else{
+            setIamges(new Array(6).fill(""))
+        }
+        
+        setAboutMe(me.aboutMe)
+        setDepartment(me.department)
+        setGender(me.gender)
+        setAge(me.age)
+    }
 
-    return {images, setIamges, aboutMe, setAboutMe,  department, setDepartment, gender, setGender, age, setAge, birth, setBirth}
+    return {images, setIamges, aboutMe, setAboutMe,  department, setDepartment, gender, setGender, age, setAge, birth, setBirth, initialize}
 }
 
 export default useNTU;
