511b92aa50070e3b1050dc3a6dfe56be1039d551
Chi-Hung, Chuang <ko841215@gmail.com>
2022-01-08 16:19:14 +0800

    frontend

--- frontend/src/graphql/mutation.js
+++ frontend/src/graphql/mutation.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 const CREATE_CHATBOX_MUTATION = gql`
-  mutation createChatBox($name1: String!, $name2: String!) {
-    createChatBox(name1: $name1, name2: $name2) {
+  mutation createChatBox($email1: String!, $email2: String!) {
+    createChatBox(email1: $email1, email2: $email2) {
       name
       messages {
         sender {
@@ -25,4 +25,13 @@ const CREATE_MESSAGE_MUTATION = gql`
   }
 `;
 
-export {CREATE_CHATBOX_MUTATION, CREATE_MESSAGE_MUTATION};
+const CREATE_LIKE_MUTATION = gql`
+  mutation createLike($from: String!, $to: String!) {
+    createLike(from: $from, to: $to) {
+      name
+      email
+    }
+  }
+`;
+
+export {CREATE_CHATBOX_MUTATION, CREATE_MESSAGE_MUTATION, CREATE_LIKE_MUTATION};
