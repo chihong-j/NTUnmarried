89e5f9f1cd4601fde9cb7b9ae8471055b031f060
Whyu1125 <kobe7682477@gmail.com>
2022-01-12 19:22:28 +0800

    delete debug message

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -38,16 +38,11 @@ const Mutation = {
         console.log(me.email);
         const user = await db.UserModel.findOne({ mail:me.email });
         console.log(user);
-        console.log("here0");
         const readStream1 = createReadStream();
         const readStream2 = createReadStream();
-        console.log("here1");
         const writeStream = await saveImage(db, readStream1, filename);
-        console.log("here2");
         user.images.push(writeStream.id);
-        console.log("here3");
         await user.save();
-        console.log("here");
         return await readStreamToDataUrl(readStream2);
     },
 
